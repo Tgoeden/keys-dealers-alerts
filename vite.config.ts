8732d0c81169869df9f552a81c3021b6f4c47abf
Tgoeden <105976703+Tgoeden@users.noreply.github.com>
2025-12-29 13:06:30 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  base: "/keys-dealers-alerts/",
   plugins: [react()],
-});
+  base: '/keys-dealers-alerts/'
+})
