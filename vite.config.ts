e03c841f4dba4560e7d6192f93978e02bc44223c
Tgoeden <105976703+Tgoeden@users.noreply.github.com>
2025-12-29 12:16:09 -0700

    Update vite.config.ts
    Fix Vite base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,7 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import react from "@vitejs/plugin-react";
 
-export default defineConfig(() => ({
+export default defineConfig({
   base: "/keys-dealers-alerts/",
-  server: {
-    host: "::",
-    port: 8080,
-  },
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+});
